f5fcdbf03f7bd5c16822aa75b4cfc140f97fc7c1
vYZ <vyz@protonmail.com>
2017-03-03 23:16:40 +0100

    Test bisection

---
+++ tests/root_finding/bisection.cpp
@@ -0,0 +1,32 @@
+#include "../catch.hpp"
+#include"../../root_finding/bisection.hpp"
+
+TEST_CASE(
+    "bisection basic tests"
+    "[root_finding][bisection]"
+    ){
+
+  class Function{
+    public:
+      constexpr Function()=default;
+      constexpr double operator()(const double x){
+        return x*x*x - x -2;
+      }
+  };
+
+  SECTION("basic bisection tests"){
+    constexpr auto result1 = numpp::root_finding::bisection(Function{}, 1., 2.);
+    constexpr auto result2 = numpp::root_finding::bisection(Function{}, 1., 2., 16);
+    constexpr auto result3 = numpp::root_finding::bisection(Function{}, 1., 2., 1000, 0.000001);
+    SECTION("correct results"){
+      REQUIRE(result2 == Approx(1.5213928));
+    }
+    SECTION("auto epsilon vs set epsilon"){
+      REQUIRE(result1 == Approx(result3));
+    }
+    SECTION("set iterations vs set epsilon"){
+      REQUIRE(result2 == Approx(result3));
+    }
+  }
+
+}
