3d0345e2b90ba3cfd51f25148b25a5b2f5a8987d
vYZ <vyz@protonmail.com>
2017-03-03 15:35:46 +0100

    Test backward differentiation

---
+++ tests/differentiation/backward_tests.cpp
@@ -0,0 +1,29 @@
+#include "../catch.hpp"
+#include "../../differentiation/backward.hpp"
+
+TEST_CASE(
+		"backward finite difference tests",
+		"[differentiation][backward]"
+    ){
+
+  class example_function{
+    public:
+      constexpr double operator()(double x){
+        return (((x*(x+4))*3)*x)
+          /((x+13.37)*7);
+      }
+  };
+  example_function f{};
+
+  SECTION("basic result tests"){
+    constexpr auto result1 = numpp::derivative::backward(f, 3.1415, 0.00001);
+    constexpr auto result2 = numpp::derivative::backward(f, 3.1415);
+    SECTION("correct results"){
+      REQUIRE(result2 == Approx(1.31001));
+    }
+    SECTION("set h vs auto h"){
+      REQUIRE(result1 == Approx(result2));
+    }
+  }
+
+}
