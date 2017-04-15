c5adf786823997ff16472458f82d37ab1138fa1f
vyzyv <vyz@protonmail.com>
2017-04-15 15:02:30 +0200

    Add vector multiplication

--- structures/vector/vector_operations.hpp
+++ structures/vector/vector_operations.hpp
@@ -6,11 +6,24 @@
 #define VECTOR_OPERATIONS_HPP_
 
 #include"vector_utils.hpp"
-#include<experimental/tuple>
+#include"../matrices/normal/normal_operations.hpp" //TEMP
+#include"tuple"
 
 namespace numpp{
-
   namespace impl{
+    namespace helper{
+      template<typename T, typename... Args>
+        constexpr auto multiply(const T& first_elem, const Args&... second){
+          return std::make_tuple((first_elem * second)...);
+        }
+
+      template<typename T, std::size_t Size, typename Tuple, std::size_t... Elements>
+        constexpr auto unpack(Tuple&& tup, std::index_sequence<Elements...>){
+          return matrix::normal<T, Size, Size>{
+            std::get<Elements>(tup) ...
+          };
+      }
+    }
     template<typename T, typename U, std::size_t Size, bool Transposition, typename Operation, std::size_t... Index>
       constexpr auto accumulate(
           const vector<T,Size,Transposition>& first,
@@ -25,8 +38,8 @@ namespace numpp{
 
     template<typename T, typename U, std::size_t Size, bool Transposition, typename Operation, std::size_t... Index>
       constexpr auto accumulate(
-          const vector<T,Size,Transposition>& vec,
-          const U scalar,
+          T&& scalar,
+          const vector<U,Size,Transposition>& vec,
           Operation&& op,
           std::index_sequence<Index...>
       ){
@@ -35,6 +48,18 @@ namespace numpp{
         };
     }
 
+    template<typename T, typename U, std::size_t Size, typename Operation, std::size_t... Index>
+      constexpr auto accumulate(
+          const vector<T,Size,false>& first,
+          const vector<U,Size,true>& second,
+          Operation&& op,
+          std::index_sequence<Index...>
+      ){
+        return helper::unpack<std::common_type_t<T,U>, Size>(
+            std::tuple_cat(helper::multiply(first(Index), second(Index)...)...),
+            std::make_index_sequence<Size*Size>{}
+            );
+      }
   }
 
   //MAIN VECTOR OPERATIONS
@@ -57,48 +82,51 @@ namespace numpp{
 		}
 
 
-	template<
-		typename T,
-		typename U,
-		std::size_t Size,
-		bool Transposition
-		/* typename = std::enable_if_t<std::is_arithmetic<U>{}> */
-	>
+	template<typename T, typename U, std::size_t Size, bool Transposition>
 		constexpr auto operator*(
 				const vector<T,Size,Transposition>& vec,
-				const U scalar
+				U&& scalar
 		){
-      return impl::accumulate(vec, scalar, std::multiplies<>{},
+      return impl::accumulate(std::forward<U>(scalar), vec, std::multiplies<>{},
           std::make_index_sequence<Size>{});
 		}
 
 
-	template<
-		typename T,
-		typename U,
-		std::size_t Size,
-		bool Transposition
-		/* typename = std::enable_if_t<std::is_arithmetic<U>{}> */
-	>
+	template<typename T,typename U, std::size_t Size, bool Transposition>
 		constexpr auto operator*(
-				const U scalar,
+				U&& scalar,
 				const vector<T,Size,Transposition>& vec
 		){
       return vec*scalar;
 		}
 
-	template<
-		typename T,
-		typename U,
-		std::size_t Size,
-		bool Transposition
-		/* typename = std::enable_if_t<std::is_arithmetic<U>{}> */
-	>
+	template<typename T,typename U, std::size_t Size>
+		constexpr auto operator*(
+				const vector<T, Size, true>& first,
+				const vector<U, Size, false>& second
+		){
+      T ret{};
+      for(std::size_t i=0; i<Size; ++i)
+        ret += first(i)*second(i);
+      return ret;
+		}
+
+	template<typename T,typename U, std::size_t Size>
+		constexpr auto operator*(
+				const vector<T, Size, false>& first,
+				const vector<U, Size, true>& second
+		){
+      return impl::accumulate(first, second, std::multiplies<>{},
+          std::make_index_sequence<Size>{}
+      );
+		}
+
+	template<typename T, typename U, std::size_t Size, bool Transposition>
 		constexpr auto operator/(
 				const vector<T,Size,Transposition>& vec,
-				const U scalar
+				U&& scalar
 		){
-      return impl::accumulate(vec, scalar, std::divides<>{},
+      return impl::accumulate(std::forward<U>(scalar), vec, std::divides<>{},
           std::make_index_sequence<Size>{});
 		}
 }
