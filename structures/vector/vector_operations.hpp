da2e3d5134a9975eeeb4ae75c451b7e9d1d6532c
vyzyv <vyz@protonmail.com>
2017-04-20 23:37:08 +0200

    Fix vector multiplication
    - Working for abitrary vector dimensions (arbitrary matrix creation)

--- structures/vector/vector_operations.hpp
+++ structures/vector/vector_operations.hpp
@@ -11,19 +11,18 @@
 
 namespace numpp{
   namespace impl{
-    namespace helper{
       template<typename T, typename... Args>
         constexpr auto multiply(const T& first_elem, const Args&... second){
           return std::make_tuple((first_elem * second)...);
         }
 
-      template<typename T, std::size_t Size, typename Tuple, std::size_t... Elements>
+      template<typename T, std::size_t Rows, std::size_t Columns, typename Tuple, std::size_t... Elements>
         constexpr auto unpack(Tuple&& tup, std::index_sequence<Elements...>){
-          return matrix::normal<T, Size, Size>{
+          return matrix::normal<T, Rows, Columns>{
             std::get<Elements>(tup) ...
           };
       }
-    }
+
     template<typename T, typename U, std::size_t Size, bool Transposition, typename Operation, std::size_t... Index>
       constexpr auto accumulate(
           const vector<T,Size,Transposition>& first,
@@ -48,21 +47,22 @@ namespace numpp{
         };
     }
 
-    template<typename T, typename U, std::size_t Size, typename Operation, std::size_t... Index>
+    template<typename T, typename U, std::size_t FirstSize, std::size_t SecondSize, typename Operation, std::size_t... Rows, std::size_t... Columns>
       constexpr auto accumulate(
-          const vector<T,Size,false>& first,
-          const vector<U,Size,true>& second,
+          const vector<T,FirstSize,false>& first,
+          const vector<U,SecondSize,true>& second,
           Operation&& op,
-          std::index_sequence<Index...>
+          std::index_sequence<Rows...>,
+          std::index_sequence<Columns...>
       ){
-        return helper::unpack<std::common_type_t<T,U>, Size>(
-            std::tuple_cat(helper::multiply(first(Index), second(Index)...)...),
-            std::make_index_sequence<Size*Size>{}
+        return unpack<std::common_type_t<T,U>, FirstSize, SecondSize>(
+            std::tuple_cat(multiply(first(Rows), second(Columns)...)...),
+            std::make_index_sequence<FirstSize*SecondSize>{}
             );
       }
   }
+  //END OF IMPLEMENTATION DETAILS
 
-  //MAIN VECTOR OPERATIONS
 	template<typename T, typename U, std::size_t Size, bool Transposition>
 		constexpr auto operator+(
 				const vector<T,Size,Transposition>& first,
@@ -111,13 +111,13 @@ namespace numpp{
       return ret;
 		}
 
-	template<typename T,typename U, std::size_t Size>
+	template<typename T,typename U, std::size_t Rows, std::size_t Columns>
 		constexpr auto operator*(
-				const vector<T, Size, false>& first,
-				const vector<U, Size, true>& second
+				const vector<T, Rows, false>& first,
+				const vector<U, Columns, true>& second
 		){
       return impl::accumulate(first, second, std::multiplies<>{},
-          std::make_index_sequence<Size>{}
+          std::make_index_sequence<Rows>{}, std::make_index_sequence<Columns>{}
       );
 		}
 
