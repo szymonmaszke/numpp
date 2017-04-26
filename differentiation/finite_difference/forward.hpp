18f6a6c696966ce4bee0798ca9d5bee4467baeef
vyzyv <vyz@protonmail.com>
2017-04-26 10:52:41 +0200

    Update differentiation to C++17

---
+++ differentiation/finite_difference/forward.hpp
@@ -0,0 +1,44 @@
+#ifndef CENTRAL_DERIVATIVE_HPP
+#define CENTRAL_DERIVATIVE_HPP
+
+#include<limits>
+#include<type_traits>
+#include<cmath>
+
+namespace numpp::derivative::finite{
+  template<typename Func,typename T>
+    constexpr auto forward(Func&& f, T&& x, T&& h){
+      return (f(x+h)-f(x))/h;
+    }
+
+  template<
+    typename Func,
+    typename T,
+    typename = std::enable_if_t<std::is_floating_point<T>::value>
+  >
+    constexpr auto forward(Func&& f, T x){
+      auto h = x*std::sqrt(std::numeric_limits<T>::epsilon());
+      return (f(x+h)-f(x))/h;
+    }
+
+  template<
+    typename Func,
+    typename T,
+    typename U,
+    typename = std::enable_if_t<
+      std::is_floating_point<T>::value and
+      std::is_arithmetic<U>::value
+    >
+  >
+    constexpr auto forward(Func&& f, T x, U y){
+      auto h = x*std::sqrt(std::numeric_limits<T>::epsilon());
+      return (f(x+h)-y)/h;
+    }
+
+  template<typename Func, typename T, typename U>
+    constexpr auto forward(Func&& f, T&& x, U&& y, T&& h){
+      return (f(x+h)-y)/h;
+    }
+}
+
+#endif
