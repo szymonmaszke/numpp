bcad3b71be5b13540c481793fc5ff6815fa7f7da
vyzyv <vyz@protonmail.com>
2017-07-16 23:43:17 +0200

    Fix forward automatic differentiation

--- differentiation/automatic/forward.hpp
+++ differentiation/automatic/forward.hpp
@@ -1,23 +1,37 @@
 #include<cmath>
-#include <iostream>
+#include<iostream>
 #include<cmath>
 #include<type_traits>
 
 //Jerzy Karczmarczuk Foton 2012 numer 117
+//ADD n-th ORDER DERIVATIVE
+//ADD cast operators
+
 namespace numpp::differentiation::automatic{
   template<typename T>
     class forward{
+
+      static_assert(std::is_arithmetic<T>(),
+          "Current implementation of forward differentiation supports only \
+          arithmetic types. Specialize is_arithmetic IF your class may be equal to 1"
+          );
+
       public:
+        using value_type = T;
+				using size_type = std::size_t;
+
         const T value;
         const T derivative;
 
-        constexpr forward(T&& value, T&& derivative = T{}):
-          value{std::forward<T>(value)},
-          derivative{std::forward<T>(derivative)}
+        constexpr forward(T value, T derivative = 1):
+            value{value},
+            derivative{derivative}
         {}
 
         constexpr forward(const forward&) = default;
         constexpr forward(forward&&) = default;
+        constexpr forward& operator=(const forward&) = default;
+        constexpr forward& operator=(forward&&) = default;
 
         constexpr auto operator+(const forward& other){
           return forward{
@@ -26,10 +40,14 @@ namespace numpp::differentiation::automatic{
           };
         }
 
-        //FIX MINUS?
         constexpr auto operator-(const forward& other){
           return forward{value-other.value, derivative-other.derivative};
         }
+
+        template<typename U>
+          constexpr auto operator*(U&& other){
+            return forward{value * other, derivative*other};
+          }
 
         constexpr auto operator*(const forward& other){
           return forward{
@@ -56,47 +74,65 @@ namespace numpp::differentiation::automatic{
 #endif
 
 namespace std{
-  CONSTEXPR auto exp(numpp::differentiation::automatic::forward<auto> number){
+
+  constexpr auto relu(const numpp::differentiation::automatic::forward<auto>& number){
+    return numpp::differentiation::automatic::forward{
+      std::max(0, number.value),
+      (((number.value) > 0) ? 1 : 0)
+    };
+  }
+
+  CONSTEXPR auto pow(
+      const numpp::differentiation::automatic::forward<auto>& number,
+      const auto& exp
+      ){
+    return numpp::differentiation::automatic::forward{
+      std::pow(number.value, exp),
+      (exp * std::pow(number.value, exp-1))
+    };
+  }
+
+  CONSTEXPR auto exp(const numpp::differentiation::automatic::forward<auto>& number){
     return numpp::differentiation::automatic::forward{
       std::exp(number.value),
       number.derivative * std::exp(number.value)
     };
   }
-  CONSTEXPR auto log(numpp::differentiation::automatic::forward<auto> number){
+  CONSTEXPR auto log(const numpp::differentiation::automatic::forward<auto>& number){
     return numpp::differentiation::automatic::forward{
       std::log(number.value),
-      number.derivative/number.value.
+      number.derivative/number.value
     };
   }
-  CONSTEXPR auto sin(numpp::differentiation::automatic::forward<auto> number){
+  CONSTEXPR auto sin(const numpp::differentiation::automatic::forward<auto>& number){
     return numpp::differentiation::automatic::forward{
       std::sin(number.value),
       number.derivative*cos(number.value)
     };
   }
 
-  CONSTEXPR auto cos(numpp::differentiation::automatic::forward<auto> number){
+  CONSTEXPR auto cos(const numpp::differentiation::automatic::forward<auto>& number){
     return numpp::differentiation::automatic::forward{
       std::cos(number.value),
       -number.derivative * sin(number.value)
     };
   }
 
-  CONSTEXPR auto arctan(numpp::differentiation::automatic::forward<auto> number){
+  CONSTEXPR auto arctan(const numpp::differentiation::automatic::forward<auto>& number){
     return numpp::differentiation::automatic::forward{
       std::arctan(number.value),
       number.derivative / (1 + number.value*number.value)
     };
   }
 
-  CONSTEXPR auto sqrt(numpp::differentiation::automatic::forward<auto> number){
+  CONSTEXPR auto sqrt(const numpp::differentiation::automatic::forward<auto>& number){
     return numpp::differentiation::automatic::forward{
       std::sqrt(number.value),
       number.derivative / (2 * std::sqrt(number.value))
     };
   }
 
-  CONSTEXPR auto asin(numpp::differentiation::automatic::forward<auto> number){
+  CONSTEXPR auto asin(const numpp::differentiation::automatic::forward<auto>& number){
     return numpp::differentiation::automatic::forward{
       std::asin(number.value),
       number.derivative / std::sqrt(1 - number*number)
@@ -108,8 +144,8 @@ namespace std{
 int main(){
   using numpp::differentiation::automatic::forward;
 
-  constexpr forward x{2.137,1.};
-  constexpr auto result = std::pow(x, 3.) * x;
+  constexpr forward<double, 1> x{1.};
+  constexpr auto result = std::pow(x, 3.) - x;
   std::cout << result.derivative << std::endl;
 
   return 0;
