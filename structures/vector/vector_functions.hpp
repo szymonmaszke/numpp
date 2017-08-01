f9ae8d3532e1be5195367e3297ee3e6484a9f45f
vyzyv <vyz@protonmail.com>
2017-08-01 16:06:34 +0200

    Add sum operations
    With those functions you don't have to perform transpotion of vector
    to obtain v' * v product.
    Smaller runtime overhead, easier to prototype functions

--- structures/vector/vector_functions.hpp
+++ structures/vector/vector_functions.hpp
@@ -9,7 +9,24 @@ namespace numpp{
       return vector<T, Size, !Transposition>{vec};
     }
 
+  template<typename T, typename U, std::size_t Size, bool TranspositionOne, bool TranspositionTwo>
+    constexpr std::common_type_t<T,U> sum(
+        const vector<T, Size, TranspositionOne>& first,
+        const vector<U, Size, TranspositionTwo>& second
+    ){
+      std::common_type_t<T,U> ret{};
+      for(std::size_t i=0; i<Size; ++i)
+        ret += first(i)*second(i);
+      return ret;
+    }
 
+  template<typename T, std::size_t Size, bool Transposition>
+    constexpr T sum(const vector<T, Size, Transposition>& vec){
+      T ret{};
+      for(std::size_t i=0; i<Size; ++i)
+        ret += vec(i)*vec(i);
+      return ret;
+    }
 }
 
 #endif
