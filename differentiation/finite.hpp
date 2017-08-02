d3666d35c1da7f3534c20314f96e2a659fbe27b6
vyzyv <vyz@protonmail.com>
2017-08-02 19:42:30 +0200

    Add summary header for easier includes

---
+++ differentiation/finite.hpp
@@ -0,0 +1,8 @@
+#ifndef NUMPP_DIFFERENTIATION_FINITE_HPP_
+#define NUMPP_DIFFERENTIATION_FINITE_HPP_
+
+#include"./finite_difference/backward.hpp"
+#include"./finite_difference/central.hpp"
+#include"./finite_difference/forward.hpp"
+
+#endif
