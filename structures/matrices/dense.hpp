9941074125345a0a8828b1355c5afe5f01dada44
vyzyv <vyz@protonmail.com>
2017-08-03 20:30:23 +0200

    Fix includes folder

--- structures/matrices/dense.hpp
+++ structures/matrices/dense.hpp
@@ -1,11 +1,11 @@
 #ifndef NUMPP_STRUCTURES_MATRICES_DENSE_HPP_
 #define NUMPP_STRUCTURES_MATRICES_DENSE_HPP_
 
-#include"./normal/functions.hpp"
-#include"./normal/mathematical.hpp"
-#include"./normal/operations.hpp"
-#include"./normal/structure.hpp"
-#include"./normal/utils.hpp"
+#include"./dense/functions.hpp"
+#include"./dense/mathematical.hpp"
+#include"./dense/operations.hpp"
+#include"./dense/structure.hpp"
+#include"./dense/utils.hpp"
 #include"../vector.hpp"
 
 #endif
