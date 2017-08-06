3d2fefe755a8b73be8fa677bea6712dadd7b3175
vyzyv <vyz@protonmail.com>
2017-08-06 02:31:10 +0200

    Make includes appropriate for docker

--- tests/speed/differentiation/central_test.py
+++ tests/speed/differentiation/central_test.py
@@ -30,10 +30,10 @@ parser.add_argument('--arghigh', '-ah', required=True,
 args = parser.parse_args()
 run_test(
         args,
-        '#include"../../../differentiation/central.hpp"\n', 5,
+        '#include"numpp/differentiation/finite.hpp"\n', 5,
         'gsl_deriv_central(&F, ', 29,
         '''constexpr auto result_numpp =
-        numpp::derivative::central(numpp_function{}, ''', 37,
+        numpp::derivative::finite::central(numpp_function{}, ''', 37,
         9, 16,
         "./differentiation/logs/central/compile"
         )
