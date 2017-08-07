1485182a68d587de9fc398073a97375dbf86651a
a395ux91 <vyz@protonmail.com>
2017-08-07 22:46:31 +0200

    Functionality tests (#42)
    * Add operator overloads
    
    * Add newton method basic tests
    
    * Add halley method basic tests

--- roots/newton.hpp
+++ roots/newton.hpp
@@ -9,71 +9,71 @@
 
 namespace numpp::roots{
   //SYMBOLIC VERSION OF THE ALGORITHM
-  template<
-    typename Function, typename Derivative,
-    typename T
-  >
-    constexpr auto newton(
-        T&& value,
-        std::size_t iterations=1000,
-        const double epsilon = 1e-7
-    ){
-        auto argument{value};
-        for(std::size_t i=0; i<iterations; ++i){
-          auto root = Function::calculate(std::experimental::make_array(argument));
-          auto derivative = Derivative::calculate(std::experimental::make_array(argument));
-          if(impl::abs(derivative) < epsilon || root < epsilon)
-            break;
-          argument = argument - root/derivative;
-        }
-        return argument;
-    }
+    template<
+      typename Function, typename Derivative,
+      typename T
+    >
+      constexpr auto newton(
+          T&& value,
+          std::size_t iterations=1000,
+          const double epsilon = 1e-7
+      ){
+          auto argument{value};
+          for(std::size_t i=0; i<iterations; ++i){
+            auto root = Function::calculate(std::experimental::make_array(argument));
+            auto derivative = Derivative::calculate(std::experimental::make_array(argument));
+            if(impl::abs(derivative) < epsilon || root < epsilon)
+              break;
+            argument = argument - root/derivative;
+          }
+          return argument;
+      }
 
   //FOR FINITE DIFFERENCE
-  template<
-    typename Function, typename Derivative,
-    typename T,
-    typename = std::enable_if_t<std::is_floating_point<T>::value>
-  >
-    constexpr auto newton(
-        Function&& f,
-        Derivative&& df,
-        T&& value,
-        std::size_t iterations=1000,
-        const double epsilon = 1e-7
-    ){
-        auto argument{value};
-        for(std::size_t i=0; i<iterations; ++i){
-          auto root = f(argument);
-          auto derivative = df(argument);
-          if(impl::abs(derivative) < epsilon || root < epsilon)
-            break;
-          argument = argument - root/derivative;
-        }
-        return argument;
+    template<
+      typename Function, typename Derivative,
+      typename T
+      /* typename = std::enable_if_t<std::is_floating_point<T>::value> */
+    >
+      constexpr auto newton(
+          Function&& f,
+          Derivative&& df,
+          T&& value,
+          std::size_t iterations=1000,
+          const double epsilon = 1e-7
+      ){
+          auto argument{value};
+          for(std::size_t i=0; i<iterations; ++i){
+            auto root = f(argument);
+            auto derivative = df(argument);
+            if(impl::abs(derivative) < epsilon || root < epsilon)
+              break;
+            argument = argument - root/derivative;
+          }
+          return argument;
     }
 
   //FOR AUTOMATIC DIFFERENTIATION METHOD
-  template<
-    typename FunctionAndDerivative,
-    typename T,
-    typename = std::enable_if_t<std::is_floating_point<T>::value>
-  >
-    constexpr auto newton(
-        T&& value,
-        FunctionAndDerivative f,
-        std::size_t iterations=1000,
-        const double epsilon = 1e-7
-    ){
-        auto argument{value};
-        for(std::size_t i=0; i<iterations; ++i){
-          auto [root, derivative] = f(argument);
-          if(impl::abs(derivative) < epsilon || root < epsilon)
-            break;
-          argument = argument - root/derivative;
-        }
-        return argument;
-    }
+    template<
+      typename FunctionAndDerivative,
+      typename T
+      /* typename = std::enable_if_t<std::is_floating_point<T>::value> */
+    >
+      constexpr auto newton(
+          FunctionAndDerivative&& f,
+          T&& value,
+          std::size_t iterations=1000,
+          const double epsilon = 1e-7
+      ){
+          auto argument{value};
+          for(std::size_t i=0; i<iterations; ++i){
+            auto [root, derivative] = f(argument);
+            if(impl::abs(derivative) < epsilon || root < epsilon)
+              break;
+            argument = argument - root/derivative;
+          }
+          return argument;
+      }
 }
 
 #endif
